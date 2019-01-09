6639691bb9cfee4f0ad5e8ac88a814ab21c8528a
Dominic Kolbe <kolbedominic@gmail.com>
2019-01-09 22:25:08 +0100

    remove api key from github and move it to env file

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-  apiKey: 'AIzaSyCbw3a5hogvqH4A6Y1h9sti_KFWNiXleaY',
-  authDomain: 'has-to-be-done.firebaseapp.com',
-  databaseURL: 'https://has-to-be-done.firebaseio.com',
-  projectId: 'has-to-be-done',
-  storageBucket: '',
-  messagingSenderId: '473661323208',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SERNDER_ID,
 };
 
 class Firebase {
