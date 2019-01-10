cfcd8eefc3650e3a5e5e6e5375e84071620fe6f6
Dominic Kolbe <kolbedominic@gmail.com>
2019-01-11 00:12:23 +0100

    CS

--- src/components/Todolist/index.js
+++ src/components/Todolist/index.js
@@ -120,13 +120,15 @@ class Todolist extends Component {
               !todo.todolistId && <TodoRow
                 key={todo.uuid}
                 className={todo.done ? 'isDone' : ''}
-                >
+              >
                 <div
                   className="doneButton"
                   onClick={e => this.onChange(todo.uuid, { done: !todo.done })}
                 >
                   {todo.done && <div className="check-icon">
-                    <svg aria-hidden="true" data-prefix="fal" data-icon="check" role="img" viewBox="0 0 448 512" class="svg-inline--fa fa-check fa-w-14 fa-lg"><path fill="currentColor" d="M413.505 91.951L133.49 371.966l-98.995-98.995c-4.686-4.686-12.284-4.686-16.971 0L6.211 284.284c-4.686 4.686-4.686 12.284 0 16.971l118.794 118.794c4.686 4.686 12.284 4.686 16.971 0l299.813-299.813c4.686-4.686 4.686-12.284 0-16.971l-11.314-11.314c-4.686-4.686-12.284-4.686-16.97 0z" class=""></path></svg>
+                    <svg aria-hidden="true" data-prefix="fal" data-icon="check" role="img" viewBox="0 0 448 512">
+                      <path fill="currentColor" d="M413.505 91.951L133.49 371.966l-98.995-98.995c-4.686-4.686-12.284-4.686-16.971 0L6.211 284.284c-4.686 4.686-4.686 12.284 0 16.971l118.794 118.794c4.686 4.686 12.284 4.686 16.971 0l299.813-299.813c4.686-4.686 4.686-12.284 0-16.971l-11.314-11.314c-4.686-4.686-12.284-4.686-16.97 0z" />
+                    </svg>
                   </div>}
                 </div>
                 <input
