f6e93ea08aaa0cc61e64a095dc23357df8aa83b0
Dominic Kolbe <kolbedominic@gmail.com>
2019-01-10 12:31:14 +0100

    create card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background: #FFF;
+  border: 1px solid #DAE1E9;
+  border-radius: 4px;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  margin: 0 0 30px 0;
+  width: 100%;
+`;
+const Header = styled.div`
+  align-items: center;
+  display: flex;
+  height: 55px;
+  padding: 0 20px;
+  h2 {
+    color: #0067C8;
+    cursor: default;
+    font-weight: 700;
+    font-size: 16px;
+    margin: 0;
+  }
+`;
+const Main = styled.div`
+  padding: 0 20px;
+  position: relative;
+`;
+
+const Card = ({ children, ...props }) => {
+  return (
+    <Container {...props}>
+      <Header>
+        <h2>{props.title}</h2>
+      </Header>
+      <Main>
+        {children}
+      </Main>
+    </Container>
+  )
+};
+
+export default Card;
