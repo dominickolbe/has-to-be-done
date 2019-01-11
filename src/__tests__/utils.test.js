49b45d6e0826d42dadcf47811e353825169efda9
Dominic Kolbe <kolbedominic@gmail.com>
2019-01-11 17:39:13 +0100

    feat: add new utils to get list by id

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,5 +1,5 @@
 import todos from '../__mock__/todos.json';
-import { filterDeleted } from '../utils';
+import { filterDeleted, getListById } from '../utils';
 
 describe('Utils', () => {
 
@@ -8,5 +8,10 @@ describe('Utils', () => {
     filtered.forEach(element => {
       expect(element.deleted).toBe(false || undefined);
     });
+  });
+
+  it('expect to get the correct todolist', () => {
+    const expectedList = getListById(todos, todos[0].uuid);
+    expect(expectedList).toBe(todos[0]);
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,3 @@
 export const filterDeleted = list => list.filter(item => !item.deleted);
+
+export const getListById = (list, uuid) => list.find(item => item.uuid === uuid);
