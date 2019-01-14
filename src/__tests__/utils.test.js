77096d89afa6ec8e31a52824d0d07bac499b786b
Dominic Kolbe <kolbedominic@gmail.com>
2019-01-14 10:57:02 +0100

    feat: update tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -7,13 +7,18 @@ Object.entries(database.todolists).forEach(
 );
 
 describe('Utils', () => {
-  it('expect to get the correct todolist', () => {
+  it('expect getListById to get the correct todolist', () => {
     const expectedList = getListById(todolists, todolists[0].uuid);
     expect(expectedList).toBe(todolists[0]);
   });
 
-  it('expect to get the correct todolist 2', () => {
+  it('expect getListById to get the correct todolist 2', () => {
     const expectedList = getListById(todolists, todolists[todolists.length -1].uuid);
     expect(expectedList).toBe(todolists[todolists.length -1]);
+  });
+
+  it('expect getListById to get null if ID not found', () => {
+    const expectedList = getListById(todolists, 'randomId');
+    expect(expectedList).toBe(null);
   });
 });
