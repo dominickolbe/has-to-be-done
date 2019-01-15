3a7959cbce60eec8b6c4d1a66798b01eda47635f
Dominic Kolbe <kolbedominic@gmail.com>
2019-01-15 15:01:56 +0100

    feat: add new utils func

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
 export const getListById = (list, uuid) => list.find(item => item.uuid === uuid) || null;
+
+export const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  return result.splice(endIndex, 0, removed);
+};
