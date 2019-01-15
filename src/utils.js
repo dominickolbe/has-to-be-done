f2b4a9b084565a49ab62d6db2934e608f76c8628
Dominic Kolbe <kolbedominic@gmail.com>
2019-01-15 15:07:13 +0100

    fix: bugfix

--- src/utils.js
+++ src/utils.js
@@ -3,5 +3,6 @@ export const getListById = (list, uuid) => list.find(item => item.uuid === uuid)
 export const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
-  return result.splice(endIndex, 0, removed);
+  result.splice(endIndex, 0, removed);
+  return result;
 };
